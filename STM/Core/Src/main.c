68deee264781e50795c47320739b0158ce4efcb5
fedeg202 <s5187919@studenti.unige.it>
2024-07-22 14:33:45 +0200

    Changed time format
    Co-Authored-By: Nicolò Trebino <92218858+nicolotrebino@users.noreply.github.com>

--- STM/Core/Src/main.c
+++ STM/Core/Src/main.c
@@ -355,40 +355,40 @@ static void MX_RTC_Init(void)
 
   /** Initialize RTC and set the Time and Date
   */
-  sTime.Hours = 0x0;
-  sTime.Minutes = 0x0;
-  sTime.Seconds = 0x0;
+  sTime.Hours = 0;
+  sTime.Minutes = 0;
+  sTime.Seconds = 0;
   sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
   sTime.StoreOperation = RTC_STOREOPERATION_RESET;
-  if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BCD) != HAL_OK)
+  if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN) != HAL_OK)
   {
     Error_Handler();
   }
   sDate.WeekDay = RTC_WEEKDAY_MONDAY;
   sDate.Month = RTC_MONTH_JANUARY;
-  sDate.Date = 0x1;
-  sDate.Year = 0x1;
+  sDate.Date = 1;
+  sDate.Year = 1;
 
-  if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BCD) != HAL_OK)
+  if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BIN) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Enable the Alarm A
   */
-  sAlarm.AlarmTime.Hours = 0x0;
-  sAlarm.AlarmTime.Minutes = 0x0;
-  sAlarm.AlarmTime.Seconds = 0x0;
-  sAlarm.AlarmTime.SubSeconds = 0x0;
+  sAlarm.AlarmTime.Hours = 0;
+  sAlarm.AlarmTime.Minutes = 0;
+  sAlarm.AlarmTime.Seconds = 0;
+  sAlarm.AlarmTime.SubSeconds = 0;
   sAlarm.AlarmTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
   sAlarm.AlarmTime.StoreOperation = RTC_STOREOPERATION_RESET;
   sAlarm.AlarmMask = RTC_ALARMMASK_DATEWEEKDAY|RTC_ALARMMASK_HOURS
                               |RTC_ALARMMASK_MINUTES;
   sAlarm.AlarmSubSecondMask = RTC_ALARMSUBSECONDMASK_ALL;
   sAlarm.AlarmDateWeekDaySel = RTC_ALARMDATEWEEKDAYSEL_DATE;
-  sAlarm.AlarmDateWeekDay = 0x1;
+  sAlarm.AlarmDateWeekDay = 1;
   sAlarm.Alarm = RTC_ALARM_A;
-  if (HAL_RTC_SetAlarm_IT(&hrtc, &sAlarm, RTC_FORMAT_BCD) != HAL_OK)
+  if (HAL_RTC_SetAlarm_IT(&hrtc, &sAlarm, RTC_FORMAT_BIN) != HAL_OK)
   {
     Error_Handler();
   }
@@ -1183,7 +1183,7 @@ void process_json_time(const char* time){
 		CurrentTime.Seconds = (uint8_t) seconds_num;
 		CurrentTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
 		CurrentTime.StoreOperation = RTC_STOREOPERATION_RESET;
-		if (HAL_RTC_SetTime(&hrtc, &CurrentTime, RTC_FORMAT_BCD) != HAL_OK)
+		if (HAL_RTC_SetTime(&hrtc, &CurrentTime, RTC_FORMAT_BIN) != HAL_OK)
 		{
 			send_uart_message("Error parsing time 1");
 			Error_Handler();
@@ -1193,7 +1193,7 @@ void process_json_time(const char* time){
 		CurrentDate.Date = (uint8_t)monthDay_num;
 		CurrentDate.Year = (uint8_t)year_num%100;
 
-		if (HAL_RTC_SetDate(&hrtc, &CurrentDate, RTC_FORMAT_BCD) != HAL_OK)
+		if (HAL_RTC_SetDate(&hrtc, &CurrentDate, RTC_FORMAT_BIN) != HAL_OK)
 		{
 			send_uart_message("Error parsing time 2");
 			Error_Handler();
@@ -1220,8 +1220,8 @@ void RTC_to_ISO8601(RTC_DateTypeDef *date, RTC_TimeTypeDef *time, char *buffer) 
 }
 
 void HAL_RTC_AlarmAEventCallback(RTC_HandleTypeDef *hrtcAlarm){
-	HAL_RTC_GetTime(&hrtc, &CurrentTime, RTC_FORMAT_BCD);
-	HAL_RTC_GetDate(&hrtc, &CurrentDate, RTC_FORMAT_BCD);
+	HAL_RTC_GetTime(&hrtc, &CurrentTime, RTC_FORMAT_BIN);
+	HAL_RTC_GetDate(&hrtc, &CurrentDate, RTC_FORMAT_BIN);
 
 	RTC_to_ISO8601(&CurrentDate,&CurrentTime,buf);
 	if (eventCount > 0){
